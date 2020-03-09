f26ccb11c7b8cf6c0fd31672c32d2fda898d6a80
Giovani Ferreira <gioferreira@gmail.com>
2020-03-09 16:30:18 -0300

    daily sketch

--- sketch_20200309a/sketch.js
+++ sketch_20200309a/sketch.js
@@ -1,7 +1,23 @@
-function setup() {
+var canvasWidth = 1920 / 2;
+var canvasHeight = 1080 / 2;
+var segments = 10;
+var pX = 0;
+var pY = 0;
 
+function setup() {
+  createCanvas(canvasWidth, canvasHeight);
+  background(255, 255, 255);
+  frameRate(10);
+  stroke(0, 20);
 }
 
 function draw() {
+  // for (let segment = 0; segment <= segments; segment++) {
+  //   let cX = noise(pX) * canvasWidth / 2;
+  //   let cY = noise(pY) * canvasHeight / 2;
+  //   line(pX, pY, cX, cY);
+  //   pX = cX;
+  //   pY = cY;
+  // }
 
 }
