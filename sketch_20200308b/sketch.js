6a843f6aa716d7534cf6f802a3bc7ca9c35639a3
Giovani Ferreira <gioferreira@gmail.com>
2020-03-08 21:35:27 -0300

    finished sketch

--- sketch_20200308b/sketch.js
+++ sketch_20200308b/sketch.js
@@ -1,7 +1,19 @@
 function setup() {
-
+  createCanvas(720, 720);
+  noCursor();
+  colorMode(HSB, 360, 100, 100);
+  rectMode(CENTER);
+  noStroke();
+  frameRate(30);
 }
 
 function draw() {
+  // let randomX = random(0, 719);
+  let randomX = (frameCount * 8) % 720;
+  // let randomY = random(0, 719);
+  let randomY = (frameCount * 8) % 720;
+  background(randomY / 2, 100, 100);
 
+  fill(360 - (randomY / 2), 100, 100);
+  rect(360, 360, randomX + 1, randomX + 1);
 }
